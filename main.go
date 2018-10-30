886e87d5e389bab584cc0b9e72cf18bc3a7fcd62
Nader Ziada <nziada@pivotal.io>
2018-10-30 16:15:01 -0400

    Add unit test

--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Hello world received a request.")
-	fmt.Fprintf(w, "Hello World! \n")
+	target := os.Getenv("TARGET")
+	if target == "" {
+		target = "world"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", Reverse(target))
 }
 
 func main() {
@@ -16,4 +21,13 @@ func main() {
 
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
+}
+
+// Reverse returns its argument string reversed rune-wise left to right.
+func Reverse(s string) string {
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r)
 }
